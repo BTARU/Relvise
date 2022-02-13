4a67e4267b694c911d062778015f623759d04616
Bulat <87946133+BTARU@users.noreply.github.com>
2022-02-13 18:24:07 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 document.addEventListener("click", scrollToElem);
 
 function scrollToElem(e) {
-    const targetElem = e.target;
+    let targetElem = e.target;
     if (targetElem.dataset.goto && document.querySelector(targetElem.dataset.goto)) {
         const gotoBlock = document.querySelector(targetElem.dataset.goto);
         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
@@ -19,4 +19,25 @@ function scrollToElem(e) {
     }
 }
 
-// Прокрутка по клику
+// Scroll arrow to top of the page (elem #arrowToTop, add attr hidden)
+
+if (document.querySelector("#arrowToTop")) {
+    window.addEventListener("scroll", function () {
+        if (scrollY > document.documentElement.clientHeight) {
+            arrowToTop.hidden = false;
+        } else arrowToTop.hidden = true;
+    });
+
+    arrowToTop.addEventListener("click", scrollToTop);
+
+    function scrollToTop(event) {
+        let targetElem = event.target.closest("#arrowToTop");
+
+        if (targetElem) {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        }
+    }
+}
